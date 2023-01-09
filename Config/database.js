130cb6dc47833d7c547985fe05a1019ff4b36224
joeljojo <joelpamphyl@gmail.com>
2023-01-09 16:00:53 +0300

    Solved TypeError Config.Server property is required

--- Config/database.js
+++ Config/database.js
@@ -1,29 +1,27 @@
-const express = require('express');
-require('dotenv').config();
-const mssql = require('mssql');
+const express = require("express");
+const dotenv = require("dotenv").config();
+const mssql = require("mssql");
 
 const database = express();
 
 //DB configureation
 const config = {
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_HOST,
-    options: {
-        trustServerCertificate: true
-    }
-
-}
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_HOST,
+  options: {
+    trustServerCertificate: true,
+  },
+};
 
 //Connect to Database
-mssql.connect(config,(err) =>{
-    if(!err){
-        console.log('Connected to Database');
-    }
-    else{
-        console.log(err);
-    }
-})
+mssql.connect(config, (err) => {
+  if (!err) {
+    console.log("Connected to Database");
+  } else {
+    console.log(err);
+  }
+});
 
-module.exports = database;
+module.exports = config;
